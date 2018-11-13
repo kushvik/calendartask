c2de76b5937e4539d0d48ee90f41d9d23311c6c5
kushvik <kushwahvikas27@gmail.com>
2018-11-14 01:16:40 +0530

    Update getbyDate.php

--- getbyDate.php
+++ getbyDate.php
@@ -1,7 +1,7 @@
 <?php
    include 'dbconfig.php';
     $eventDate = $_POST['eventDate'];
-    $query="select * from `tbl1` where `Taskdate` = '" . $eventDate . "'";
+    $query="select * from `tbl1` where `date` = '" . $eventDate . "'";
     $result = mysqli_query($conn,$query) or die('Unsuccessful: ' . mysql_error());
     $eventResult = array();
     while ($row = mysqli_fetch_assoc($result)) {
